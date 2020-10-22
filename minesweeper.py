594421030e5869085ebec7737060300a210b34de
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2020-10-22 22:13:30 +0530

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,4 +1,5 @@
 # minesweeper in python using pygame
+# I am using bees in honor of Daniel Shiffman and Bees And Bombs , instead of the traditional mines...
 
 
 import pygame
@@ -20,16 +21,24 @@ pygame.display.set_caption('Minesweeper')
 
 grid = numpy.empty(shape = (width // rez, height // rez))
 # grid = []
+
+# for i in range(width // rez):
+#     grid.append([])
+#     for j in range(height // rez):
+#         grid[i].append(0)
+
+
 bees = []
 
 # using '_' because I have no need to save the info in a variable
-for _ in range(numbees + 1):
+for _ in range(numbees):
     i = random.randint(0, (width // rez) - 1)
     j = random.randint(0, (height // rez) - 1)
     grid[i][j] = -1
     bees.append([i, j])
 
 
+# neighbour checking algorithm...
 for i in range(len(grid)):
     for j in range(len(grid[0])):
         if grid[i][j] == -1:
@@ -62,10 +71,23 @@ def main():
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if [get_mouse_pos()[0] // rez, get_mouse_pos()[1] // rez] in bees:
-                    pygame.draw.circle(display, (169, 169, 169), (get_mouse_pos()[0] + rez // 2, get_mouse_pos()[1] + rez // 2), rez // 4)
-                else:
-                    text = font.render(str(grid[get_mouse_pos()[0] // rez][get_mouse_pos()[1] // rez]).replace('.0', ''), True, (255, 255, 255))
+                    for i in range(len(grid)):
+                        for j in range(len(grid[0])):
+                            if grid[i][j] == -1:
+                                pygame.draw.rect(display, (190, 190, 190), (i * rez, j * rez, rez, rez))
+                                pygame.draw.circle(display, (51, 51, 51), ((i * rez) + rez // 2, (j * rez) + rez // 2), rez // 4)
+
+                            elif (grid[i][j] >= 0 and grid[i][j] != -1):
+                                text = font.render(str(grid[i][j]).replace('.0', ''), True, (169, 169, 169))
+                                display.blit(text, (((i * rez) + rez // 2) - 4, ((j * rez) + rez // 2) - 6))
+    
+
+                elif grid[get_mouse_pos()[0] // rez][get_mouse_pos()[1] // rez] >= 1:
+                    text = font.render(str(grid[get_mouse_pos()[0] // rez][get_mouse_pos()[1] // rez]).replace('.0', ''), True, (169, 169, 169))
                     display.blit(text, ((get_mouse_pos()[0] + rez // 2) - 4, (get_mouse_pos()[1] + rez // 2) - 6))
+
+                else:
+                    print('That is a zero')
 
         for i in range(1, width // rez):
             pygame.draw.line(display, (169,  169, 169), (0, i * rez), (width, i * rez))
@@ -78,3 +100,9 @@ def main():
 
 
 main()
+
+
+# pygame.draw.rect(display, (190, 190, 190), (get_mouse_pos()[0], get_mouse_pos()[1], rez, rez))
+# pygame.draw.circle(display, (51, 51, 51), (get_mouse_pos()[0] + rez // 2, get_mouse_pos()[1] + rez // 2), rez // 4)
+
+# I am still working on the feature to reveal the whole block when it is a empty cell(floodfill-ish algorithm)...
